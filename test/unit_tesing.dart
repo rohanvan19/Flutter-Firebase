7cf752a87df45b7ded01ea5d5fe2fd89f00dec64
rohanvan19 <86397039+rohanvan19@users.noreply.github.com>
2022-04-24 21:10:56 +0530

    Delete unit_tesing.dart

--- test/unit_tesing.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:project_firebase/main.dart';
-
-void main(){
-  test('All fields are null', () {
-  });
-}
